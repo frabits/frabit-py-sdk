bca3e7d48f2d013f3f699368e906cc2d8dc9ea8e
blylei <blylei.info@gmail.com>
2024-02-29 00:08:07 +0800

    init

---
+++ frabit/__init__.py
@@ -0,0 +1,11 @@
+# (c) 2020 frabit-server Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of frabit-server
+#
+import os
+import sys
+import re
+import logging
+
+_logger = logging.getLogger(__name__)
