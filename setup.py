3fdbb3da7aba06b3b2594dea119e14322a1bda1c
blylei <blylei.info@gmail.com>
2024-03-02 22:54:20 +0800

    init

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#	https://www.gnu.org/licenses/gpl-3.0.txt
+# https://www.gnu.org/licenses/gpl-3.0.txt
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
